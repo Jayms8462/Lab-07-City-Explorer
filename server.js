2ae0603687e7e657cfbd57befb4819cca98aacd2
jayms8462 <jayms.portune@gmail.com>
2019-08-16 18:27:38 -0700

    Fixed ESLINT Errors

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 // const client = require('pg');
 const cors = require('cors');
-const superagent = require("superagent");
+const superagent = require('superagent');
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
@@ -20,62 +20,63 @@ app.get('/weather', searchWea);
 app.get('/events', searchEve)
 
 function Location(query, data) {
-    this.search_query = query; //outputs what was entered into field
-    this.formatted_query = data.body.results[0].formatted_address; //outputs the formatted_query in JSON AKA the City, State, Country
-    this.latitude = data.body.results[0].geometry.location.lat; //outputs the Latitude of the element in JSON
-    this.longitude = data.body.results[0].geometry.location.lng; //outputs the Longitude of the element in JSON
+  this.search_query = query; //outputs what was entered into field
+  this.formatted_query = data.body.results[0].formatted_address; //outputs the formatted_query in JSON AKA the City, State, Country
+  this.latitude = data.body.results[0].geometry.location.lat; //outputs the Latitude of the element in JSON
+  this.longitude = data.body.results[0].geometry.location.lng; //outputs the Longitude of the element in JSON
 }
 
 function searchLoc(request, response) {
-    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
-    superagent.get(url)
-        .then(result => {
-            response.send(new Location(request.query.data, result));
-        })
-        .catch(error => eventError(error, response))
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
+  console.log(url);
+  superagent.get(url)
+    .then(result => {
+      response.send(new Location(request.query.data, result));
+    })
+    .catch(error => eventError(error, response))
 
 }
 
 function Weather(data) {
-    this.forecast = data.summary;
-    this.time = new Date(data.time * 1000).toString().slice(0, 15);
+  this.forecast = data.summary;
+  this.time = new Date(data.time * 1000).toString().slice(0, 15);
 }
 
 function searchWea(request, response) {
-    const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
-    superagent.get(url)
-        .then(result => {
-            // console.log(result.body.daily.data);
-            const wea = result.body.daily.data.map(
-                test => new Weather(test)
-            );
-            response.send(wea);
-        })
-        .catch(error => eventError(error, response))
+  const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
+  superagent.get(url)
+    .then(result => {
+      // console.log(result.body.daily.data);
+      const wea = result.body.daily.data.map(
+        test => new Weather(test)
+      );
+      response.send(wea);
+    })
+    .catch(error => eventError(error, response))
 }
 
 function Event(data) {
-    this.link = data.url;
-    this.name = data.name.text;
-    this.event_date = new Date(data.start.local).toString().slice(0, 15);
-    this.summary = data.summary;
+  this.link = data.url;
+  this.name = data.name.text;
+  this.event_date = new Date(data.start.local).toString().slice(0, 15);
+  this.summary = data.summary;
 }
 
 function searchEve(request, response) {
-    const url = `https://www.eventbriteapi.com/v3/events/search?token=${process.env.EVENTBRITE_API_KEY}&location.address=${request.query.data.search_query}`;
-    superagent.get(url)
-        .then(result => {
-            const events = result.body.events.map(data => {
-                return new Event(data);
-            });
-            response.send(events);
-        })
-        .catch(error => eventError(error, response))
+  const url = `https://www.eventbriteapi.com/v3/events/search?token=${process.env.EVENTBRITE_API_KEY}&location.address=${request.query.data.search_query}`;
+  superagent.get(url)
+    .then(result => {
+      const events = result.body.events.map(data => {
+        return new Event(data);
+      });
+      response.send(events);
+    })
+    .catch(error => eventError(error, response))
 }
 
-function eventError() {
-    console.error(error);
-    if (response) {
-        response.status(500).send("Sorry, something went wrong here.");
-    }
+function eventError(error, response) {
+  console.error(error);
+  if (response) {
+    response.status(500).send('Sorry, something went wrong here.');
+  }
 }
